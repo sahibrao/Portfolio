08d30fa06d004400534c976110460b2cf045b74c
sahibrao <sahibrao@students.cs.ubc.ca>
2023-07-29 13:34:24 -0700

    added skills

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   About,
   Contact,
   Experience,
+  Skills,
   Hero,
   Navbar,
   Tech,
@@ -20,15 +21,21 @@ const App = () => {
 
         <div className="bg-about bg-cover bg-center bg-no-repeat">
           <About />
-        </div>
+        </div> 
+        
+        <Projects />
+
+      
+        <Skills />
+       
 
         <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
           <Tech />
         </div>
 
-        <Projects />
 
-        <div
+
+        {/* <div
           className="bg-experience bg-cover bg-center bg-no-repeat 
             rounded-tl-[150px] rounded-br-[150px]">
           <div
@@ -36,7 +43,7 @@ const App = () => {
             bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
             <Experience />
           </div>
-        </div>
+        </div> */}
         <div className="relative z-0">
           <Contact />
         </div>
---
+++ src/components/Skills.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+// const ServiceCard = ({ index, title, icon }) => {
+//   return (
+//     <motion.div
+//       variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+//       className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+//       <div
+//         options={{
+//           max: 45,
+//           scale: 1,
+//           speed: 450,
+//         }}
+//         className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+//         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+//         <h3 className="text-taupe text-[18px] font-bold text-center">
+//           {title}
+//         </h3>
+//       </div>
+//     </motion.div>
+//   );
+// };
+
+const Skills = () => {
+  return (
+    <div className="-mt-[6rem] justify-center">
+      <motion.div variants={textVariant()}>
+        {/* <p className={styles.sectionSubText}>Introduction</p> */}
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[33px] max-w-6xl leading-[45px]">
+          Born in Vancouver, BC. Grew up in Hong Kong and moved back to Canada to pursue my dream in Software Development. <br></br>
+          Currently pursuing a Major in Computer Science alongside a Minor in Commerce at The University of British Columbia.  <br></br>
+          In my spare time, I love attending hackathons, meeting other amazing developers, and at the gym, keeping myself fit enough to run away from any bug.  <br></br>
+           <br></br>
+      </motion.p>
+      
+     
+{/* 
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default SectionWrapper(Skills, 'skills');
