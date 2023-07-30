bfbcc83a81a9459b2c6d8ca63272b6181118d3bf
sahibrao <sahibrao@students.cs.ubc.ca>
2023-07-29 17:31:08 -0700

    added resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import resume from "/Users/sahibrao/Desktop/Projects/Portfolio-Template/src/components/SahibRao_Resume.pdf";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,42 +38,47 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[33px] max-w-6xl leading-[45px]">
-          Born in Vancouver, BC. Grew up in Hong Kong and moved back to Canada to pursue my dream in Software Development. <br></br>
-          Currently pursuing a Major in Computer Science alongside a Minor in Commerce at The University of British Columbia.  <br></br>
-          In my spare time, I love attending hackathons, meeting other amazing developers, and at the gym, keeping myself fit enough to run away from any bug.  <br></br>
-           <br></br>
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[33px] max-w-6xl leading-[45px]"
+      >
+        Born in Vancouver, BC. Grew up in Hong Kong and moved back to Canada to
+        pursue my dream in Software Development. <br></br>
+        Currently pursuing a Major in Computer Science alongside a Minor in
+        Commerce at The University of British Columbia. <br></br>
+        In my spare time, I love attending hackathons, meeting other amazing
+        developers, and at the gym, keeping myself fit enough to run away from
+        any bug. <br></br>
+        <br></br>
       </motion.p>
-      
+
       {/* TODO: needs a download picture */}
-      <button 
-            className="live-demo flex justify-center sm:gap-4 
+      <button
+        className="live-demo flex justify-center sm:gap-4 
             gap-3 sm:text-[30px] text-[45px] text-timberWolf 
             font-bold font-beckman items-center py-5
             whitespace-nowrap sm:w-[170px] sm:h-[70px] 
             w-[250px] h-[120px]  bg-jetLight 
             hover:bg-battleGray hover:text-eerieBlack 
             transition duration-[0.2s] ease-in-out "
-              onClick={() =>
-                window.open(
-                  'SahibRao_Resume.pdf', //paste the link to your resume here
-                  '_blank'
-                )
-              }
-              onMouseOver={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', downloadHover);
-              }}
-              onMouseOut={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', download);
-              }}>
-          Resume
-          
-           {/* <img
+        onClick={() =>
+          // window.open(
+          //   "resume", //paste the link to your resume here
+          //   "_blank"
+          // )
+          window.open(resume)
+        }
+        onMouseOver={() => {
+          document
+            .querySelector(".download-btn")
+            .setAttribute("src", downloadHover);
+        }}
+        onMouseOut={() => {
+          document.querySelector(".download-btn").setAttribute("src", download);
+        }}
+      >
+        Resume
+        {/* <a href={resume} target="_blank"></a> */}
+        {/* <img
                 src={download}
                 alt="download"
                 className="download-btn sm:w-[26px] sm:h-[26px] 
@@ -87,4 +95,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
